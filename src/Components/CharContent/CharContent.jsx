554706942e022b6ab9c9f146b9f9a7414d68b921
Pulupenko Alexey <optycat@gmail.com>
2023-02-02 11:07:55 +0200

    CharContent refactor

--- src/Components/CharContent/CharContent.jsx
+++ src/Components/CharContent/CharContent.jsx
@@ -1,15 +1,27 @@
+import { Component } from 'react';
+
 import CharList from './CharList/CharList';
 import CharInfo from './CharInfo/CharInfo';
 
 import './charContent.scss';
 
-const CharContent = () => {
-    return (
-        <div className="char__content">
-            <CharList/>
-            <CharInfo/>
-        </div>
-    );
+class CharContent extends Component {
+    state = {
+        selectedChar: null
+    }
+
+    onCharSelected = (id) => {
+        this.setState({ selectedChar: id });
+    }
+
+    render() {
+        return (
+            <div className="char__content">
+                <CharList onCharSelected={this.onCharSelected}/>
+                <CharInfo />
+            </div>
+        );
+    }
 }
 
 export default CharContent;
